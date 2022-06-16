e8e9f0a5a16ca38793b2a4d5b77203943ebfb3d8
mfschmidt <mikeschmidt@schmidtgracen.com>
2022-06-15 20:26:11 -0400

    new file to share libraries with importers

---
+++ starelib/__init__.py
@@ -0,0 +1,7 @@
+from . import plotting
+from . import util
+from . import vascular_cluster
+from . import wrapper
+
+# Sphinx autodoc seems to need the above imports to traverse all of these
+# modules implicitly.
