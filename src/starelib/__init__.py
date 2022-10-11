c8d924f2b4e266927411ceac3de759bbd2596d4b
mfschmidt <mikeschmidt@schmidtgracen.com>
2022-10-10 22:21:36 -0400

    cleanup

--- src/starelib/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import plotting
-from . import util
-from . import vascular_cluster
-from . import wrapper
-
-# Sphinx autodoc seems to need the above imports to traverse all of these
-# modules implicitly.
