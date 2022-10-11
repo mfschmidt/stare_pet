1f8d14f9aaaafc6d0bdd2d8310e0aeef4c152390
mfschmidt <mikeschmidt@schmidtgracen.com>
2022-10-10 22:22:39 -0400

    added a features dict and self-naming

--- src/starelib/centroid.py
+++ src/starelib/centroid.py
@@ -11,16 +11,24 @@ class Centroid(TimeActivityCurve):
                  label,  # should be non-zero as zero indicates background
                  k,
                  labels,
+                 name=None,
                  vascular=False,
                  best_in_k=False,
                  best_overall=False, ):
         """ Centroid constructor """
 
         # Specified properties
-        super().__init__(activity, timepoints, "k-means")
+        super().__init__(activity, timepoints, "k-means", name=name)
         self.label = label  # int, one of k clusters
         self.k = k  # int, how many clusters
         self.labels = labels  # ndarray shaped like (1000000,)
         self.vascular = vascular
         self.best_in_k = best_in_k
         self.best_overall = best_overall
+
+        # Each centroid can also contain {label: value} features,
+        # initialized as an empty dict.
+        # An example would be
+        # {"likely_noise": True, "likely_irreversible": False}
+        # to label this centroid with those features.
+        self.features = {}
