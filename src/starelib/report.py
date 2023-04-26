a5db263dde6dde3f2cf19d675578c25327d3c0f1
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2023-04-25 22:34:59 -0400

    expand report and section with more functions

--- src/starelib/report.py
+++ src/starelib/report.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import logging
+from pathlib import Path
+import numpy as np
 
 
 class Section:
@@ -21,30 +23,42 @@ class Section:
     def end(self):
         self._end_datetime = datetime.now()
 
+    def duration(self):
+        if self._start_datetime is None or self._end_datetime is None:
+            return 0
+        else:
+            return self._end_datetime - self._start_datetime
+
     def add_line(self, line):
         self.items.append(f"<p>{line}</p>\n")
         self._report.logger.info(line)
 
     def add_figure(self, fig_path, caption):
-        img_tag = f"<img src=\"{fig_path}\" style=\"width: 90%\">"
+        rel_path = str(Path(fig_path).relative_to(self._report.path.parent))
+        img_tag = f"<img src=\"{rel_path}\" style=\"width: 90%\">"
         caption_tag = f"<figcaption>{caption}</figcaption>"
         img_html = "\n".join([
-            f"<a href=\"{fig_path}\">",
+            f"<a href=\"{rel_path}\">",
             "<figure>", img_tag, caption_tag, "</figure>",
             "</a>",
         ])
         self.items.append(f"<p>{img_html}</p>")
 
+    def add_table(self, dataframe):
+        self.items.append(f"<p>{dataframe.to_html()}</p>")
+
     def html(self):
         if self._end_datetime is None:
             return f"<h2>{self.title} not yet complete.</h2>"
         else:
+            relative_start = self._start_datetime - self._report.start_time
+            duration = self._end_datetime - self._start_datetime
             return "\n".join([
                 f"<h2>{self.title}</h2>",
                 "<p>Started at {} ({} in) and took {}</p>".format(
                     self._start_datetime.strftime(self._dt_format),
-                    self._start_datetime - self._report.start_time,
-                    self._end_datetime - self._start_datetime,
+                    str(relative_start).split('.', 2)[0],
+                    str(duration).split('.', 2)[0],
                 ),
             ] + self.items) + "\n"
 
@@ -53,11 +67,12 @@ class Report:
     """ A class to keep track of events in time and write a final report.
     """
 
-    def __init__(self, title, logger=None, dt_format="%Y-%m-%d_%I-%M"):
+    def __init__(self, title, file, logger=None, dt_format="%Y-%m-%d %I:%M:%S"):
         self._start_datetime = datetime.now()
         self._end_datetime = None
         self._dt_format = dt_format
         self.title = title
+        self.path = Path(file).resolve()
         self.sections = []
         if logger is None:
             self.logger = logging.getLogger(title)
@@ -65,11 +80,15 @@ class Report:
             self.logger = logger
 
     def __str__(self):
-        return "A report"
+        return (f"A report, titled \"{self.title}\", "
+                f"with {len(self.sections)} sections")
 
     @property
     def start_time(self):
         return self._start_datetime
+
+    def end(self):
+        self._end_datetime = datetime.now()
 
     def begin_section(self, title):
         new_section = Section(title, self, dt_format=self._dt_format)
@@ -78,6 +97,13 @@ class Report:
         return new_section
 
     def write(self, file):
+        total_duration = np.sum([_.duration() for _ in self.sections])
         with open(file, "w") as f:
+            f.write(f"<h1>{self.title}</h1>\n")
             for sect in sorted(self.sections, key=lambda x: x.start_time):
                 f.write(sect.html())
+            f.write("<footer>"
+                    f"completed {self._end_datetime.strftime(self._dt_format)},"
+                    f" taking {str(total_duration).split('.', 2)[0]}"
+                    "</footer>\n")
+            f.write("\n")
